16da8e90b44aa161e7af5dcf1e8449e58639d979
Mike Danese <mikedanese@google.com>
2016-09-20 10:51:12 -0700

    add release script for debian to be called by jenkins

---
+++ debian/jenkins.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+set -o nounset
+set -o xtrace
+
+
+declare -r BUILD_TAG="$(date '+%y%m%d%H%M%S')"
+declare -r IMG_NAME="debian-builder-${DEB_CHANEL}:${BUILD_TAG}"
+declare -r DEB_RELEASE_BUCKET="gs://kubernetes-release-dev/debian/${DEB_CHANEL}"
+
+function cleanup() {
+  docker rmi "${IMG_NAME}" || true
+}
+
+trap cleanup EXIT SIGINT SIGTERM
+
+docker build -t "${IMG_NAME}" debian/
+docker run --rm -v "${PWD}/bin:/src/bin" "${IMG_NAME}"
+
+gsutil -m cp -nc bin/* "${DEB_RELEASE_BUCKET}/${BUILD_TAG}/"
+echo "${BUILD_NUMBER}" | gsutil -m cp - "${DEB_RELEASE_BUCKET}/latest"
