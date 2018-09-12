01e32d189176f2ef10893eb9287e58c4c997c77d
Adam Stokes <battlemidget@users.noreply.github.com>
2018-09-12 11:17:39 -0400

    Fix docker builds (#29)
    * Fix docker builds
    
    This updates to a recent stable container for building snaps via docker.
    
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>
    
    * Include file in container, link magic db
    
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- snap/docker-build.sh
+++ snap/docker-build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-docker run --rm -v $PWD:/root/snap -w /root/snap -e SNAPCRAFT_SETUP_CORE=1\
-    snapcraft/xenial-amd64 make "$@"
+docker run --rm -v "$PWD":/root/snap -w /root/snap \
+       -e SNAPCRAFT_SETUP_CORE=1 \
+       -e KUBE_ARCH=amd64 \
+       -e KUBE_VERSION=$(curl -L https://dl.k8s.io/release/stable.txt) \
+       -v /usr/bin/curl:/usr/bin/curl \
+       -v /usr/bin/make:/usr/bin/make \
+       -v /usr/share/misc/magic.mgc:/usr/share/misc/magic.mgc \
+       -v /usr/bin/file:/usr/bin/file \
+       -v /usr/lib/x86_64-linux-gnu/libmagic.so.1:/usr/lib/x86_64-linux-gnu/libmagic.so.1 \
+       snapcore/snapcraft:stable \
+       ./build-scripts/build "$@"
