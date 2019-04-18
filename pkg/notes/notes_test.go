cc75a6b45952d5118fa196f67999988f4a3aa79d
Sascha Grunert <sgrunert@suse.com>
2019-04-18 14:54:27 +0200

    Remove unused code from notes test
    Signed-off-by: Sascha Grunert <sgrunert@suse.com>

--- pkg/notes/notes_test.go
+++ pkg/notes/notes_test.go
@@ -11,18 +11,6 @@ import (
 	"golang.org/x/oauth2"
 )
 
-const (
-	v1_10_0        = "fc32d2f3698e36b93322a3465f63a14e9f0eaead"
-	v1_11_0_alpha1 = "4761788b2afa42a4573a6794902eb93fe666d5c5"
-	v1_11_0_alpha2 = "ed9b25c90241b2b8a1fa10b96381c57f99ca952a"
-	v1_11_0_beta1  = "4e3b2843df571c3b80c834d7c23bc6da1a22aab8"
-	v1_11_0_beta2  = "be2cfcf9e44b5162a294e977329d6c8194748c4e"
-	v1_11_0_rc1    = "8745ea56e3f1f3ad20050c1762eb6ba6f7786675"
-	v1_11_0_rc2    = "d0a17cb4bbdf608559f257a76acfaa9acb054903"
-	v1_11_0_rc3    = "931fc3b3aef9d679436978529fc7065d75352671"
-	v1_11_0        = "91e7b4fd31fcd3d5f436da26c980becec37ceefe"
-)
-
 func githubClient(t *testing.T) *github.Client {
 	token, tokenSet := os.LookupEnv("GITHUB_TOKEN")
 	if !tokenSet {
