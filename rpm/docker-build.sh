8dc3992116f7c4edc8d1016135c97f3dc00455cd
Peter Swica <peter.swica@outlook.com>
2019-07-01 20:06:38 -0400

    Cleaning docker-build so that it passes shellcheck

--- rpm/docker-build.sh
+++ rpm/docker-build.sh
@@ -5,14 +5,14 @@ docker build -t kubelet-rpm-builder .
 echo "Cleaning output directory..."
 sudo rm -rf output/*
 mkdir -p output
-docker run -ti --rm -v $PWD/output/:/root/rpmbuild/RPMS/ kubelet-rpm-builder $1
-sudo chown -R $USER $PWD/output
+docker run -ti --rm -v "$PWD/output/:/root/rpmbuild/RPMS/" kubelet-rpm-builder "$1"
+sudo chown -R "$USER" "$PWD/output"
 
 echo
 echo "----------------------------------------"
 echo
 echo "RPMs written to: "
-ls $PWD/output/*/
+ls "$PWD/output/*/"
 echo
 echo "Yum repodata written to: "
-ls $PWD/output/*/repodata/
+ls "$PWD/output/*/repodata/"
