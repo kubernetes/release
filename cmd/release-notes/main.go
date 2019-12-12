2ea789828a7b1687356f779ed31db6428d720a2b
Sascha Grunert <sgrunert@suse.com>
2019-12-12 12:09:52 +0100

    Add support for patch-to-patch discovery mode
    This adds the patch release discovery method to the release notes
    generator where it is now possible to generate the release notes by
    only setting the appropriate release branch:
    
    ```
    > go run cmd/release-notes/main.go \
        --github-token <token> \
        --branch release-1.16
        --discover patch-to-patch
    ```
    
    This results in an auto-discovery of the correct start and end SHAs:
    
    ```
    INFO Parsing latest tag v1.16.4
    INFO Parsing previous tag v1.16.3
    INFO discovered start SHA b3cbbae08ec52a7fc73d334838e18d17e8512749
    INFO discovered end SHA 224be7bdce5a9dd0c2fd0d46b83865648e2fe0ba
    ```
    
    Signed-off-by: Sascha Grunert <sgrunert@suse.com>

--- cmd/release-notes/main.go
+++ cmd/release-notes/main.go
@@ -60,6 +60,7 @@ var opts = &options{}
 const (
 	revisionDiscoveryModeNONE          = "none"
 	revisionDiscoveryModeMinorToLatest = "minor-to-latest"
+	revisionDiscoveryModePatchToPatch  = "patch-to-patch"
 )
 
 var cmd = &cobra.Command{
@@ -196,6 +197,7 @@ func init() {
 			strings.Join([]string{
 				revisionDiscoveryModeNONE,
 				revisionDiscoveryModeMinorToLatest,
+				revisionDiscoveryModePatchToPatch,
 			}, ", "),
 		),
 	)
@@ -342,7 +344,7 @@ func validateOptions(*cobra.Command, []string) error {
 	}
 
 	// Check if we want to automatically discover the revisions
-	if opts.discoverMode == revisionDiscoveryModeMinorToLatest {
+	if opts.discoverMode != revisionDiscoveryModeNONE {
 		repo, err := git.CloneOrOpenGitHubRepo(
 			opts.repoPath,
 			opts.githubOrg,
@@ -352,10 +354,20 @@ func validateOptions(*cobra.Command, []string) error {
 		if err != nil {
 			return err
 		}
-		start, end, err := repo.LatestNonPatchFinalToLatest()
-		if err != nil {
-			return err
+
+		var start, end string
+		if opts.discoverMode == revisionDiscoveryModeMinorToLatest {
+			start, end, err = repo.LatestNonPatchFinalToLatest()
+			if err != nil {
+				return err
+			}
+		} else if opts.discoverMode == revisionDiscoveryModePatchToPatch {
+			start, end, err = repo.LatestPatchToPatch(opts.branch)
+			if err != nil {
+				return err
+			}
 		}
+
 		opts.startSHA = start
 		opts.endSHA = end
 		logrus.Infof("discovered start SHA %s", start)
