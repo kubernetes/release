c481be31a12d588d5c81c40bd67d30b391d76de6
Stephen Augustus <saugustus@vmware.com>
2020-02-11 20:11:32 -0500

    repo-infra: Fix boilerplate errors
    Signed-off-by: Stephen Augustus <saugustus@vmware.com>

--- lib/gitlib.sh
+++ lib/gitlib.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-#
-# Copyright 2016 The Kubernetes Authors All rights reserved.
+# Copyright 2016 The Kubernetes Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+
 ###############################################################################
 # GIT-related constants and functions
 
