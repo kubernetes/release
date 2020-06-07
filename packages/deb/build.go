d415c0f0213917e7bc0cacacffd4384c11a537b7
Stephen Augustus <saugustus@vmware.com>
2020-06-07 10:49:31 -0400

    packages: Update Bazel files and OWNERS
    Signed-off-by: Stephen Augustus <saugustus@vmware.com>

--- packages/deb/build.go
+++ packages/deb/build.go
@@ -14,6 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+//nolint
+// This file is intended for legacy support and should not be linted
 package main
 
 import (
