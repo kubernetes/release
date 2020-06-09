65e5e9e35547aa90f9d3c77a03235284a9ed2132
Carlos Panato <ctadeu@gmail.com>
2020-06-09 16:58:23 +0200

    schedule-builder: add some unit tests

---
+++ cmd/schedule-builder/cmd/markdown.go
@@ -0,0 +1,57 @@
+/*
+Copyright 2020 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/olekukonko/tablewriter"
+	"github.com/sirupsen/logrus"
+)
+
+func parseSchedule(patchSchedule PatchSchedule) string {
+	output := []string{}
+	output = append(output, "### Timeline\n")
+	for _, releaseSchedule := range patchSchedule.Schedules {
+		output = append(output, fmt.Sprintf("### %s\n", releaseSchedule.Release),
+			fmt.Sprintf("Next patch release is **%s**\n", releaseSchedule.Next),
+			fmt.Sprintf("End of Life for **%s** is **%s**\n", releaseSchedule.Release, releaseSchedule.EndOfLifeDate))
+
+		tableString := &strings.Builder{}
+		table := tablewriter.NewWriter(tableString)
+		table.SetAutoWrapText(false)
+		table.SetHeader([]string{"Patch Release", "Cherry Pick Deadline", "Target Date"})
+		table.Append([]string{strings.TrimSpace(releaseSchedule.Next), strings.TrimSpace(releaseSchedule.CherryPickDeadline), strings.TrimSpace(releaseSchedule.TargetDate)})
+
+		for _, previous := range releaseSchedule.PreviousPatches {
+			table.Append([]string{strings.TrimSpace(previous.Release), strings.TrimSpace(previous.CherryPickDeadline), strings.TrimSpace(previous.TargetDate)})
+		}
+		table.SetBorders(tablewriter.Border{Left: true, Top: false, Right: true, Bottom: false})
+		table.SetCenterSeparator("|")
+		table.Render()
+
+		output = append(output, tableString.String())
+	}
+
+	scheduleOut := strings.Join(output, "\n")
+
+	logrus.Info("Schedule parsed")
+	println(scheduleOut)
+
+	return scheduleOut
+}
---
+++ cmd/schedule-builder/cmd/root_test.go
@@ -0,0 +1,120 @@
+/*
+Copyright 2020 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestFailRootCommand(t *testing.T) {
+	err := rootCmd.Execute()
+	require.Error(t, err)
+}
+
+const expectedOut = `### Timeline
+
+### 1.18
+
+Next patch release is **1.18.4**
+
+End of Life for **1.18** is **TBD**
+
+| PATCH RELEASE | CHERRY PICK DEADLINE | TARGET DATE |
+|---------------|----------------------|-------------|
+| 1.18.4        | 2020-06-12           | 2020-06-17  |
+| 1.18.3        | 2020-05-15           | 2020-05-20  |
+| 1.18.2        | 2020-04-13           | 2020-04-16  |
+
+### 1.17
+
+Next patch release is **1.17.7**
+
+End of Life for **1.17** is **TBD**
+
+| PATCH RELEASE | CHERRY PICK DEADLINE | TARGET DATE |
+|---------------|----------------------|-------------|
+| 1.17.7        | 2020-06-12           | 2020-06-17  |
+| 1.17.6        | 2020-05-15           | 2020-05-20  |
+
+### 1.16
+
+Next patch release is **1.16.11**
+
+End of Life for **1.16** is **TBD**
+
+| PATCH RELEASE | CHERRY PICK DEADLINE | TARGET DATE |
+|---------------|----------------------|-------------|
+| 1.16.11       | 2020-06-12           | 2020-06-17  |
+| 1.16.10       | 2020-05-15           | 2020-05-20  |
+| 1.16.9        | 2020-04-13           | 2020-04-16  |
+`
+
+func TestRun(t *testing.T) {
+	testcases := []struct {
+		name    string
+		options *options
+		expect  func(error, string)
+	}{
+		{
+			name: "should parse successufuly",
+			options: &options{
+				configPath: "testdata/schedule.yaml",
+			},
+			expect: func(err error, out string) {
+				// checks the error of run func call
+				require.Nil(t, err)
+
+				// compare the output generated with the expected
+				outFile, errFile := ioutil.ReadFile(out)
+				require.NoError(t, errFile)
+				require.Equal(t, string(outFile), expectedOut)
+			},
+		},
+		{
+			name: "should fail parsing",
+			options: &options{
+				configPath: "testdata/bad_schedule.yaml",
+			},
+			expect: func(err error, out string) {
+				// checks the error of run func call
+				require.NotNil(t, err)
+
+				// should not create the output file
+				_, errFile := os.Stat(out)
+				require.True(t, os.IsNotExist(errFile))
+			},
+		},
+	}
+
+	for tcCount, tc := range testcases {
+		t.Logf("Test case: %s", tc.name)
+
+		tempDir, err := ioutil.TempDir("/tmp", "schedule-test")
+		require.Nil(t, err)
+		defer os.RemoveAll(tempDir)
+
+		tc.options.outputFile = fmt.Sprintf("%s/output-%d.md", tempDir, tcCount)
+
+		err = run(tc.options)
+		tc.expect(err, tc.options.outputFile)
+	}
+}
