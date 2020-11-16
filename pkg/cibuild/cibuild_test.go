12309232c67b663008b0c5f97f9a77c3f105bd27
Stephen Augustus <saugustus@vmware.com>
2020-11-16 05:32:33 -0500

    pkg/cibuild: Add more test cases
    Signed-off-by: Stephen Augustus <saugustus@vmware.com>

--- pkg/cibuild/cibuild_test.go
+++ pkg/cibuild/cibuild_test.go
@@ -28,7 +28,7 @@ import (
 
 var err = errors.New("error")
 
-var testVersionTag string = "v1.20.0"
+var testVersion string = "v1.20.0-beta.1.655+d20e3246bade17"
 
 type testStateParameters struct {
 }
@@ -89,6 +89,108 @@ func TestBuild(t *testing.T) {
 		sut.SetImpl(mock)
 
 		err := sut.Build()
+
+		if tc.shouldError {
+			require.NotNil(t, err)
+		} else {
+			require.Nil(t, err)
+		}
+	}
+}
+
+func TestIsKubernetesRepo(t *testing.T) {
+	for _, tc := range []struct {
+		prepare          func(*cibuildfakes.FakeImpl)
+		isKubernetesRepo bool
+		shouldError      bool
+	}{
+		{ // success
+			prepare: func(mock *cibuildfakes.FakeImpl) {
+				mock.IsKubernetesRepoReturns(true, nil)
+			},
+			isKubernetesRepo: true,
+			shouldError:      false,
+		},
+		{ // IsKubernetesRepo fails
+			prepare: func(mock *cibuildfakes.FakeImpl) {
+			},
+			isKubernetesRepo: false,
+			shouldError:      false,
+		},
+	} {
+		sut := cibuild.NewDefaultBuild()
+
+		// TODO: Populate logic
+		sut.SetState(
+			generateTestingState(&testStateParameters{}),
+		)
+
+		mock := &cibuildfakes.FakeImpl{}
+
+		tc.prepare(mock)
+		sut.SetImpl(mock)
+
+		isK8sRepo, err := sut.IsKubernetesRepo()
+
+		require.Equal(t, tc.isKubernetesRepo, isK8sRepo)
+
+		if tc.shouldError {
+			require.NotNil(t, err)
+		} else {
+			require.Nil(t, err)
+		}
+	}
+}
+
+func TestCheckBuildExists(t *testing.T) {
+	for _, tc := range []struct {
+		prepare     func(*cibuildfakes.FakeImpl)
+		shouldError bool
+	}{
+		{ // success
+			prepare: func(mock *cibuildfakes.FakeImpl) {
+				mock.GetWorkspaceVersionReturns(testVersion, nil)
+			},
+			shouldError: false,
+		},
+		{ // GetWorkspaceVersion fails with err
+			prepare: func(mock *cibuildfakes.FakeImpl) {
+				mock.GetWorkspaceVersionReturns("", err)
+			},
+			shouldError: true,
+		},
+		{ // GetWorkspaceVersion fails on empty version
+			prepare: func(mock *cibuildfakes.FakeImpl) {
+				mock.GetWorkspaceVersionReturns("", nil)
+			},
+			shouldError: true,
+		},
+		{ // GetGCSBuildPaths fails with err
+			prepare: func(mock *cibuildfakes.FakeImpl) {
+				mock.GetGCSBuildPathsReturns([]string{}, err)
+			},
+			shouldError: true,
+		},
+		{ // ImagesExist fails with err
+			prepare: func(mock *cibuildfakes.FakeImpl) {
+				mock.ImagesExistReturns(false, err)
+			},
+			shouldError: true,
+		},
+	} {
+		sut := cibuild.NewDefaultBuild()
+
+		// TODO: Populate logic
+		sut.SetState(
+			generateTestingState(&testStateParameters{}),
+		)
+
+		mock := &cibuildfakes.FakeImpl{}
+
+		tc.prepare(mock)
+		sut.SetImpl(mock)
+
+		_, err := sut.CheckBuildExists()
 
 		if tc.shouldError {
 			require.NotNil(t, err)
