c8e8a17c92ff4a0d4f41290d2e3dcd507e28d188
Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>
2021-11-09 20:35:13 -0600

    Additional tests for spdx package
    This commit adds more testing to the spdx package which currently has
    a low coverage. These new tests sover the following functions:
    
     spdx.GetImageReferences
     spdx.TestPullImagesToArchive
     spdx.TestGetDirectoryTree
     spdx.TestIgnorePatterns
    
    Signed-off-by: Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>

--- pkg/spdx/spdx_unit_test.go
+++ pkg/spdx/spdx_unit_test.go
@@ -17,10 +17,12 @@ limitations under the License.
 package spdx
 
 import (
+	"archive/tar"
 	"bytes"
 	"compress/gzip"
 	"encoding/base64"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -56,22 +58,20 @@ func TestBuildIDString(t *testing.T) {
 }
 
 func TestUnitExtractTarballTmp(t *testing.T) {
-	tar := writeTestTarball(t)
-	require.NotNil(t, tar)
-	defer os.Remove(tar.Name())
+	tarFile := writeTestTarball(t)
+	require.NotNil(t, tarFile)
+	defer os.Remove(tarFile.Name())
 
 	sut := NewSPDX()
 	_, err := sut.ExtractTarballTmp("lsdjkflskdjfl")
 	require.NotNil(t, err)
-	dir, err := sut.ExtractTarballTmp(tar.Name())
+	dir, err := sut.ExtractTarballTmp(tarFile.Name())
 	require.Nil(t, err, "extracting file")
 	defer os.RemoveAll(dir)
 
 	require.True(t, util.Exists(filepath.Join(dir, "/text.txt")), "checking directory")
 	require.True(t, util.Exists(filepath.Join(dir, "/subdir/text.txt")), "checking subdirectory")
 	require.True(t, util.Exists(dir), "checking directory")
-
-	// Check files
 }
 
 func TestReadArchiveManifest(t *testing.T) {
@@ -104,14 +104,14 @@ func TestReadArchiveManifest(t *testing.T) {
 }
 
 func TestPackageFromTarball(t *testing.T) {
-	tar := writeTestTarball(t)
-	require.NotNil(t, tar)
-	defer os.Remove(tar.Name())
+	tarFile := writeTestTarball(t)
+	require.NotNil(t, tarFile)
+	defer os.Remove(tarFile.Name())
 
 	sut := spdxDefaultImplementation{}
 	_, err := sut.PackageFromTarball("lsdkjflksdjflk", &TarballOptions{})
 	require.NotNil(t, err)
-	pkg, err := sut.PackageFromTarball(tar.Name(), &TarballOptions{})
+	pkg, err := sut.PackageFromTarball(tarFile.Name(), &TarballOptions{})
 	require.Nil(t, err)
 	require.NotNil(t, pkg)
 
@@ -162,7 +162,7 @@ func TestExtDocReadSourceFile(t *testing.T) {
 
 func writeTestTarball(t *testing.T) *os.File {
 	// Create a testdir
-	tar, err := os.CreateTemp(os.TempDir(), "test-tar-*.tar.gz")
+	tarFile, err := os.CreateTemp(os.TempDir(), "test-tar-*.tar.gz")
 	require.Nil(t, err)
 
 	tardata, err := base64.StdEncoding.DecodeString(testTar)
@@ -176,9 +176,9 @@ func writeTestTarball(t *testing.T) *os.File {
 	require.Nil(t, err)
 
 	require.Nil(t, os.WriteFile(
-		tar.Name(), bindata, os.FileMode(0o644)), "writing test tar file",
+		tarFile.Name(), bindata, os.FileMode(0o644)), "writing test tar file",
 	)
-	return tar
+	return tarFile
 }
 
 func TestRelationshipRender(t *testing.T) {
@@ -260,3 +260,133 @@ xvnOBTcsF59eCmZT5Cz+yXT/5bX/pMb3P030fw4rlB8AAAAAAAAAAAAAAAAA4CccAXRRwL4AKAAA
 
 var sampleManifest = `[{"Config":"386bcf5c63de46c7066c42d4ae1c38af0689836e88fed37d1dca2d484b343cf5.json","RepoTags":["k8s.gcr.io/kube-apiserver-amd64:v1.22.0-alpha.1"],"Layers":["23e140cb8e03a12cba4ac571d9a7143cf5e2e9b72de3b33ce3243b4f7ad6a188/layer.tar","48dd73ececdf0f52a174ad33a469145824713bd2b73c6257ce1ba8502003ad4e/layer.tar","d397673d78556210baa112013c960cb95a3fd452e5c4a2ead2b26e5a458cd87f/layer.tar"]}]
 `
+
+func TestGetImageReferences(t *testing.T) {
+	references, err := getImageReferences("k8s.gcr.io/kube-apiserver:v1.23.0-alpha.3")
+	images := map[string]struct {
+		arch string
+		os   string
+	}{
+		"k8s.gcr.io/kube-apiserver@sha256:a82ca097e824f99bfb2b5107aa9c427633f9afb82afd002d59204f39ef81ae70": {"amd64", "linux"},
+		"k8s.gcr.io/kube-apiserver@sha256:2a11e07f916b5982d9a6e3bbf5defd66ad50359c00b33862552063beb6981aec": {"arm", "linux"},
+		"k8s.gcr.io/kube-apiserver@sha256:18f97b8c1c9b7b35dea7ba122d86e23066ce347aa8bb75b7346fed3f79d0ea21": {"arm64", "linux"},
+		"k8s.gcr.io/kube-apiserver@sha256:1a61b61491042e2b1e659c4d57d426d01d9467fb381404bff029be4d00ead519": {"ppc64le", "linux"},
+		"k8s.gcr.io/kube-apiserver@sha256:3e98f1591a5052791eec71d3c5f5d0fa913140992cb9e1d19fd80a158305c2ff": {"s390x", "linux"},
+	}
+	require.NoError(t, err)
+	// This image should have 5 architectures
+	require.Len(t, references, 5)
+	for _, refData := range references {
+		_, ok := images[refData.Digest]
+		require.True(t, ok, fmt.Sprintf("Image not found %s", refData.Digest))
+		require.Equal(t, images[refData.Digest].os, refData.OS)
+		require.Equal(t, images[refData.Digest].arch, refData.Arch)
+	}
+
+	// Test a sha reference. This is the linux/ppc64le image
+	singleRef := "k8s.gcr.io/kube-apiserver@sha256:1a61b61491042e2b1e659c4d57d426d01d9467fb381404bff029be4d00ead519"
+	references, err = getImageReferences(singleRef)
+	require.NoError(t, err)
+	require.Len(t, references, 1)
+	require.Equal(t, singleRef, references[0].Digest)
+
+	// Tag with a single image. Image 1.0 is a single image
+	references, err = getImageReferences("k8s.gcr.io/pause:1.0")
+	require.NoError(t, err)
+	require.Len(t, references, 1)
+	require.Equal(t, "k8s.gcr.io/pause@sha256:a78c2d6208eff9b672de43f880093100050983047b7b0afe0217d3656e1b0d5f", references[0].Digest)
+}
+
+func TestPullImagesToArchive(t *testing.T) {
+	impl := spdxDefaultImplementation{}
+
+	// First. If the tag does not represent an image, expect an error
+	_, err := impl.PullImagesToArchive("k8s.gcr.io/pause:0.0", "/tmp")
+	require.Error(t, err)
+
+	// Create a temp workdir
+	dir, err := os.MkdirTemp("", "extract-image-")
+	require.NoError(t, err)
+	defer os.RemoveAll(dir)
+
+	// The pause 1.0 image is a single image
+	images, err := impl.PullImagesToArchive("k8s.gcr.io/pause:1.0", dir)
+	require.NoError(t, err)
+	require.Len(t, images, 1)
+	require.FileExists(t, filepath.Join(dir, "a78c2d6208eff9b672de43f880093100050983047b7b0afe0217d3656e1b0d5f.tar"))
+
+	foundFiles := []string{}
+	expectedFiles := []string{
+		"sha256:350b164e7ae1dcddeffadd65c76226c9b6dc5553f5179153fb0e36b78f2a5e06",
+		"a3ed95caeb02ffe68cdd9fd84406680ae93d633cb16422d00e8a7c22955b46d4.tar.gz",
+		"4964c72cd0245a7f77da38425dc98b472b2699ba6c49d5a9221fb32b972bc06b.tar.gz",
+		"manifest.json",
+	}
+	tarFile, err := os.Open(filepath.Join(dir, "a78c2d6208eff9b672de43f880093100050983047b7b0afe0217d3656e1b0d5f.tar"))
+	require.NoError(t, err)
+	defer tarFile.Close()
+	tarReader := tar.NewReader(tarFile)
+	for {
+		header, err := tarReader.Next()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+		}
+		if header == nil {
+			break
+		}
+		foundFiles = append(foundFiles, header.Name)
+	}
+	require.Equal(t, expectedFiles, foundFiles)
+}
+
+func TestGetDirectoryTree(t *testing.T) {
+	dir, err := os.MkdirTemp("", "dir-tree-")
+	require.NoError(t, err)
+	defer os.RemoveAll(dir)
+
+	// Create a directory. This same array elements should be read back
+	// from the temporary directory.
+	files := []string{"test.txt", "sub1/test2.txt", "sub2/subsub/test3.txt"}
+	path := ""
+	for _, f := range files {
+		path = filepath.Join(dir, f)
+		dir := filepath.Dir(path)
+		require.NoError(t, os.MkdirAll(dir, os.FileMode(0o0755)))
+		require.NoError(t, os.WriteFile(path, []byte("test"), os.FileMode(0o644)))
+	}
+
+	impl := spdxDefaultImplementation{}
+	readFiles, err := impl.GetDirectoryTree(dir)
+	require.NoError(t, err)
+	// Now, compare contents of th array is the same
+	require.ElementsMatch(t, files, readFiles)
+}
+
+func TestIgnorePatterns(t *testing.T) {
+	dir, err := os.MkdirTemp("", "dir-tree-")
+	require.NoError(t, err)
+	defer os.RemoveAll(dir)
+	impl := spdxDefaultImplementation{}
+
+	// First, a dir without a gitignore should return no patterns, but not err
+	p, err := impl.IgnorePatterns(dir, []string{}, false)
+	require.NoError(t, err)
+	require.Len(t, p, 0)
+
+	// If we pass an extra pattern, we should get it back
+	p, err = impl.IgnorePatterns(dir, []string{".vscode"}, false)
+	require.NoError(t, err)
+	require.Len(t, p, 1)
+
+	// Now put a gitignore and read it back
+	require.NoError(t, os.WriteFile(
+		filepath.Join(dir, ".gitignore"),
+		[]byte("# NFS\n.nfs*\n\n# OSX leaves these everywhere on SMB shares\n._*\n\n# OSX trash\n.DS_Store\n"),
+		os.FileMode(0o755),
+	))
+	p, err = impl.IgnorePatterns(dir, nil, false)
+	require.NoError(t, err)
+	require.Len(t, p, 4)
+}
