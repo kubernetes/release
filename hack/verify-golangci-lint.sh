f720664f1ebe5672be059440249c2321f6795dca
Vinayak Goyal <vinayakankugoyal@gmail.com>
2022-01-27 19:16:26 +0530

    Add checksum validation for the golangci-lint install script. (#2378)
    * Use docker image instead of using curl to download and instal golangci-lint.
    
    * Update dependencies.yaml
    
    Co-authored-by: Sascha Grunert <sgrunert@redhat.com>
    
    Co-authored-by: Sascha Grunert <sgrunert@redhat.com>

--- hack/verify-golangci-lint.sh
+++ hack/verify-golangci-lint.sh
@@ -21,6 +21,13 @@ set -o pipefail
 VERSION=v1.43.0
 URL_BASE=https://raw.githubusercontent.com/golangci/golangci-lint
 URL=$URL_BASE/$VERSION/install.sh
+# If you update the version above you might need to update the checksum
+# if it does not match. We say might because in the past the install script
+# has been unchanged even if there is a new verion of golangci-lint.
+# To obtain the checksum, download the install script and run the following
+# command:
+# > sha256sum <path-to-install-script>
+INSTALL_CHECKSUM=294771225087ee48c8e0a45a99ac82ed8f9c6e9d384e692ab201986479c8594f
 
 if [[ ! -f .golangci.yml ]]; then
     echo 'ERROR: missing .golangci.yml in repo root' >&2
@@ -28,8 +35,16 @@ if [[ ! -f .golangci.yml ]]; then
 fi
 
 if ! command -v golangci-lint; then
-    curl -sfL $URL | sh -s $VERSION
-    PATH=$PATH:bin
+    INSTALL_SCRIPT=$(mktemp -d)/install.sh
+    curl -sfL $URL > $INSTALL_SCRIPT
+    if echo "${INSTALL_CHECKSUM} $INSTALL_SCRIPT" | sha256sum --check; then
+        chmod 755 $INSTALL_SCRIPT
+        $INSTALL_SCRIPT $VERSION
+        PATH=$PATH:bin
+    else
+        echo 'ERROR: install script sha256 checksum invalid' >&2
+        exit 1
+    fi
 fi
 
 golangci-lint version
