b3034211c979feba1d287b6f7e408f514562c0aa
Kubernetes Release Robot <k8s-release-robot@users.noreply.github.com>
2022-08-31 17:40:02 -0500

    Add test for release_notes. This adds 3 tests for runReleaseNotes function
    Signed-off-by: Rodolfo Martínez Vega <rodomar@outlook.com>

--- cmd/krel/cmd/release_notes_test.go
+++ cmd/krel/cmd/release_notes_test.go
@@ -17,13 +17,75 @@ limitations under the License.
 package cmd
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+	"strings"
 	"testing"
 
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
+func TestRunReleaseNotes(t *testing.T) {
+	t.Setenv("GITHUB_TOKEN", "token")
+
+	for _, tc := range []struct {
+		name        string
+		args        []string
+		shouldError bool
+		output      string
+		err         error
+	}{
+		{
+			name:        "should show release-notes help",
+			args:        []string{"release-notes"},
+			shouldError: false,
+			output:      "The 'release-notes' subcommand of krel has been developed to:",
+			err:         nil,
+		},
+		{
+			name:        "should fail since --fork flag was not specified",
+			args:        strings.Split("release-notes --create-draft-pr", " "),
+			shouldError: true,
+			output:      "",
+			err:         fmt.Errorf("validating command line options: cannot generate the Release Notes PR without --fork"),
+		},
+		{
+			name:        "should fail since user doesn't have valid credentials",
+			args:        strings.Split("release-notes --create-draft-pr --fork=myuser", " "),
+			shouldError: true,
+			output:      "",
+			err:         fmt.Errorf("while checking kubernetes/sig-release fork: while checking if repository is a fork of kubernetes/sig-release: checking if repository is a fork: getting repository: GET https://api.github.com/repos/myuser/sig-release: 401 Bad credentials []"),
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			logrus.SetOutput(io.Discard)
+			buf := new(bytes.Buffer)
+			rootCmd.SetOut(buf)
+			rootCmd.SetErr(buf)
+			rootCmd.SetArgs(tc.args)
+			err := rootCmd.Execute()
+			output := buf.String()
+
+			// fmt.Println("===> err:", err)
+			// fmt.Println("===> output:", output, "<===")
+
+			if tc.shouldError {
+				require.NotNil(t, err)
+				assert.Contains(t, output, tc.output)
+				assert.Contains(t, err.Error(), tc.err.Error())
+			} else {
+				require.Nil(t, err)
+				assert.Contains(t, output, tc.output)
+			}
+		})
+	}
+}
+
 func TestValidate(t *testing.T) {
-	// // Validate if GITHUB_TOKEN is set
+	// Validate if GITHUB_TOKEN is set
 	err := releaseNotesOpts.Validate()
 	require.NotNil(t, err)
 
