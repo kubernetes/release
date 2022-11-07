22c6b4827363adfa7ed3cdc0713ad085d6bda7af
upodroid <cy@borg.dev>
2022-11-07 19:14:09 +0000

    strip newline

--- pkg/notes/document/document_test.go
+++ pkg/notes/document/document_test.go
@@ -568,5 +568,5 @@ func makeReleaseNote(kind notes.Kind, markdown string) *notes.ReleaseNote {
 func readFile(t *testing.T, path string) string {
 	b, err := os.ReadFile(path)
 	require.NoError(t, err, "Reading file %q", path)
-	return string(b)
+	return strings.TrimSpace(string(b))
 }
