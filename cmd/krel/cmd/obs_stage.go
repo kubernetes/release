7dc1ce6d208149cde29f182816b385de3a2c96f2
Marko Mudrinić <mudrinic.mare@gmail.com>
2023-06-01 15:23:42 +0200

    Fix typo in obs_stage.go
    Signed-off-by: Marko Mudrinić <mudrinic.mare@gmail.com>

--- cmd/krel/cmd/obs_stage.go
+++ cmd/krel/cmd/obs_stage.go
@@ -27,7 +27,7 @@ import (
 	"k8s.io/release/pkg/release"
 )
 
-// stageCmd represents the subcommand for `krel stage`
+// obsStageCmd represents the subcommand for `krel obs stage`
 var obsStageCmd = &cobra.Command{
 	Use:   "stage",
 	Short: "Stage packages for a new Kubernetes version or Kubernetes subproject",
