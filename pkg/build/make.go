50583df3ff4526eb05e8e3b22dbfa6d733434474
Jim Angel <jimangel@users.noreply.github.com>
2023-09-21 18:14:30 -0500

    reverting parallel builds freal
    In an attempt to release 1.29 alpha, parallel builds result in cloud build OOM. This PR removes the parallelism as far as I can tell.

--- pkg/build/make.go
+++ pkg/build/make.go
@@ -86,8 +86,8 @@ func (m *Make) MakeCross(version string) error {
 	// Unset the build memory requirement for parallel builds
 	// TODO: Remove this once the 1.20 release reaches EOL.
 	const buildMemoryKey = "KUBE_PARALLEL_BUILD_MEMORY"
-	logrus.Infof("Unsetting %s to force parallel build", buildMemoryKey)
-	os.Setenv(buildMemoryKey, "0")
+	logrus.Infof("Setting %s to force serial build", buildMemoryKey)
+	os.Setenv(buildMemoryKey, "32")
 
 	logrus.Info("Building binaries")
 	if err := m.impl.Command(
